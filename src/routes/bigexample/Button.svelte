c081097de88035a92f2794d5dd41244bc5bfb87f
Mark Connelly <com0004@gmail.com>
2023-11-21 09:38:10 +1100

    Update events mechanism

--- src/routes/bigexample/Button.svelte
+++ src/routes/bigexample/Button.svelte
@@ -4,13 +4,18 @@
 
 	let { type = 'button', ...btnAttrs } = $prop.group<HTMLButtonAttributes>();
 	let loading = $prop(false);
+
+
 </script>
 
 <button
 	{...btnAttrs}
 	{type}
 	class="px-2 py-1 bg-red-500 rounded hover:bg-red-400 text-white active:scale-95 flex gap-2 items-center disabled:bg-gray-400 disabled:cursor-not-allowed !scale-100"
+	on:*
 >
+	<!-- ^^^ alt: on:click,mouseover,mousedown,mouseenter -->
+	<!-- ^^^ alt: on:buttonEvents -->
 	{#if loading}
 		<LoadingSpinner class="w-5" />
 	{/if}
